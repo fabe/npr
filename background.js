69185896fd24b5f4b3ff00a8fcb1766cd1a71c41
Leo Herzog <xd1936@gmail.com>
2018-08-29 15:44:16 -0400

    Add Support for AAC Stream

--- background.js
+++ background.js
@@ -1,21 +1,13 @@
 const npr = document.getElementById('npr');
 
 function start() {
-  reset();
   npr.play();
 }
 
 function stop() {
   npr.pause();
-  reset();
 }
 
 function checkIfPlaying() {
   return !npr.paused;
-}
-
-function reset() {
-  const stream = npr.src;
-  npr.src = '';
-  npr.src = stream;
 }
