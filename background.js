2af2f5dff0860ac90ba0c23969ca663f60e74c90
Leo Herzog <xd1936@gmail.com>
2018-08-30 11:44:49 -0400

    Fix removed reset function

--- background.js
+++ background.js
@@ -1,13 +1,21 @@
 const npr = document.getElementById('npr');
 
 function start() {
+  reset();
   npr.play();
 }
 
 function stop() {
   npr.pause();
+  reset();
 }
 
 function checkIfPlaying() {
   return !npr.paused;
+}
+
+function reset() {
+  const stream = npr.innerHTML;
+  npr.innerHTML = '';
+  npr.innerHTML = stream;
 }
